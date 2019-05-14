575abc7345b08adcd6bbc470ac6ba7358cf4e874
Cleverton Inácio <clev.inacio@hotmail.com>
2019-05-14 10:22:05 -0300

    Alteração funções LinkedList

--- EDB/edb_aula_17/DoubleLinkedList.h
+++ EDB/edb_aula_17/DoubleLinkedList.h
@@ -20,7 +20,6 @@ private:
     Node<Item>* last_;
 
 public:
-    DoubleLinkedList();
     void push_back(Item item);
     void push_front(Item item);
     void insert(Item item, size_t posititon);
@@ -81,51 +80,90 @@ void DoubleLinkedList<Item>::push_front(Item item){
 }
 
 template<typename Item>
-void DoubleLinkedList<Item>::insert(Item item, size_t posititon){
+void DoubleLinkedList<Item>::insert(Item item, size_t position){
 
 }
 
 template<typename Item>
 bool DoubleLinkedList<Item>::empty(){
-    Node<Item>* current  = head_;
-    Node<Item>* previous = last_;
-
-    return current== nullptr&&previous== nullptr;
+    return head_ == nullptr && last_ == nullptr;
 }
 
 template<typename Item>
 Item DoubleLinkedList<Item>::pop_back(){
 
+    Node<Item> *tmp = last_;
+    last_ = last_->prev;
+    last_->next = nullptr;
+    delete tmp;
+
+    return last_->info;
 }
 
 template<typename Item>
 Item DoubleLinkedList<Item>::pop_front(){
+    Node<Item> *tmp = head_;
+    head_ = head_->next;
+    head_->prev = nullptr;
+    delete tmp;
 
+    return head_->info;
 }
 
 template<typename Item>
-Item DoubleLinkedList<Item>::remove(size_t posititon){
+Item DoubleLinkedList<Item>::remove(size_t position){
+    if (empty()){
+        return;
+    }
 
+    Node<Item>* current = head_;
+    size_t sizeNode = size();
+    for (size_t i = 0;i<sizeNode && current!=nullptr && i!=position; i++){
+        if (current==nullptr){
+            return nullptr;
+        }
+        
+
+    }
 }
 
 template<typename Item>
 Item& DoubleLinkedList<Item>::front(){
-
+     return head_->info;
 }
 
 template<typename Item>
 Item& DoubleLinkedList<Item>::back(){
-
+    return last_->info;
 }
 
 template<typename Item>
 size_t DoubleLinkedList<Item>::size(){
+    Node<Item> *current = head_;
+    size_t qtty = 0;
 
+    while(current!=nullptr){
+        Node<Item> *tmp = current->next;
+        current = tmp;
+        qtty++;
+    }
+    return qtty;
 }
 
 template<typename Item>
 Node<Item>* DoubleLinkedList<Item>::search(Item i){
-
+    Node<Item>* current = head_;
+    while (current!=nullptr&&current->info!=i)
+    {
+        if (current==nullptr)
+        {
+            return nullptr;
+        }
+        
+        Node<Item> *tmp    = current->next;
+             current = tmp;
+    }
+    return current;
 }
 
 template<typename Item>
--- EDB/edb_aula_17/main.cpp
+++ EDB/edb_aula_17/main.cpp
@@ -4,6 +4,34 @@
 #include "DoubleLinkedList.h"
 
 int main(){
+    DoubleLinkedList<int> *l1 = new DoubleLinkedList<int>();
+
+    std::cout << l1->empty() << '\n' << '\n';
     
+    l1->push_back(1);
+    l1->push_front(5);
+    l1->push_back(3);
+
+    std:: cout << l1->front() << '\n';
+    std:: cout << l1->back() << '\n';
+    std::cout << l1->size() << '\n' << '\n';
+
+    std:: cout << l1->empty() << '\n' << '\n';
+
+    Node<int> *result = l1->search(10);
+
+    std::cout << result->info << '\n' << '\n';
+
+    std::cout << l1->pop_front() << '\n' << '\n';
+
+    std:: cout << l1->front() << '\n';
+    std:: cout << l1->back() << '\n';
+    std::cout << l1->size() << '\n' << '\n';
+    
+    std::cout << l1->pop_back() << '\n' << '\n';
+    
+    std:: cout << l1->front() << '\n';
+    std:: cout << l1->back() << '\n';
+    std::cout << l1->size() << '\n';
     return 0;
 }
